88d63b8363dbf9218da8fbab81729490d5f01c98
chenxino <chen.xin.c@qq.com>
2021-07-08 20:52:14 +0800

    726

--- daily/930.py
+++ daily/930.py
@@ -2,6 +2,19 @@ from typing import List
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
         count = 0
+
+        if goal == 0:
+            cc = 0
+            for i in range(len(nums)):
+                if nums[i] == 0:
+                    cc += 1
+                else:
+                    for c in range(cc):
+                        count += c+1
+                    cc = 0
+            for c in range(cc):
+                count += c+1
+            return count
         now_count = 0
         end = 0
         for j in range(len(nums)):
@@ -24,44 +37,24 @@ class Solution:
             return 0
         for i in range(1, len(nums)):
             if nums[i-1] == 1:
-                if goal == 0:
-                    for j in range(end+1, len(nums)):
-                        if sum(nums[i:j+1]) == goal:
-                            now_count = 1
-                            h = 1
-                            end = j
-                            while j+h < len(nums):
-
-                                if (nums[j+h]) == 0:
-                                    now_count += 1
-                                    end = j+h
-                                    h += 1
-                                else:
-                                    break
-                            break
-                else:
-                    now_count = 0
-                    for j in range(end+1, len(nums)):
-                        if nums[j] == 1:
-                            now_count += 1
-                            end = j
-                            h = 1
-                            while j+h < len(nums):
-                                if (nums[j+h]) == 0:
-                                    now_count += 1
-                                    h += 1
-                                    end = j+h
-                                else:
-                                    break
-                            break
-                        if end+1 == len(nums):
-                            break
-            elif goal == 0:
-                now_count -= 1
-            if end < i:
-                end  = i
+                now_count = 0
+                for j in range(end+1, len(nums)):
+                    if nums[j] == 1:
+                        now_count += 1
+                        end = j
+                        h = 1
+                        while j+h < len(nums):
+                            if (nums[j+h]) == 0:
+                                now_count += 1
+                                end = j+h
+                                h += 1
+                            else:
+                                break
+                        break
+                    if end+1 == len(nums):
+                        break
             print(now_count)
             count += now_count
         return count
 
-print(Solution().numSubarraysWithSum([1,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0], 0))
+print(Solution().numSubarraysWithSum([0,0,0,1,0,1,0,1,1,1,0,0,1,0,1,1,0,1,1,0], 3))
