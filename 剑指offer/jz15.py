d9b2975070d6dc48889c39dcc3fb1a67879db177
chenxino <chen.xin.c@qq.com>
2021-10-03 22:12:32 +0800

    jz15

--- 剑指offer/jz15.py
+++ 剑指offer/jz15.py
@@ -4,3 +4,12 @@ class Solution:
         while n:
             n = (n-1)&n
         return count
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        flag = 1
+        for i in range(32):
+            if n & flag:
+                count += 1
+            flag = flag << 1
+        return count
