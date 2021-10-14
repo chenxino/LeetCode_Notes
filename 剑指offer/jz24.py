9b90b1755d922a6748eb9f763c8ab4b517b83ae1
chenxino <chen.xin.c@qq.com>
2021-10-14 22:32:28 +0800

    jz24

---
+++ 剑指offer/jz24.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        def recur(cur, pre):
+            if not cur: return pre     # 终止条件
+            res = recur(cur.next, cur) # 递归后继节点
+            cur.next = pre             # 修改节点引用指向
+            return res                 # 返回反转链表的头节点
+        
+        return recur(head, None)       # 调用递归并返回
