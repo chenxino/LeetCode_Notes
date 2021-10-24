f12bd34d24e35b80e57539c2b249c447c90efb70
chenxino <chen.xin.c@qq.com>
2021-10-24 16:05:53 +0800

    jz34

--- 剑指offer/jz32.py
+++ 剑指offer/jz32.py
@@ -24,4 +24,16 @@ class Solution:
         while n_list != []:
             n_list = my_level(n_list)
         return results
-                
+        
+# 大佬 写法 用队列
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[int]:
+        if not root: return []
+        res, queue = [], collections.deque()
+        queue.append(root)
+        while queue:
+            node = queue.popleft()
+            res.append(node.val)
+            if node.left: queue.append(node.left)
+            if node.right: queue.append(node.right)
+        return res
