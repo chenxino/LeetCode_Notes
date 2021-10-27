5d8d4fdb80c6e541385a5cbaa573b79d19a90497
chenxino <chen.xin.c@qq.com>
2021-10-27 20:44:17 +0800

    jz35

--- 剑指offer/jz35.py
+++ 剑指offer/jz35.py
@@ -33,3 +33,40 @@ class Solution:
             now = now.next
             n = n.next
         return h
+
+# 不用哈希表
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        if head is None:
+            return None
+        def clone_node(head):
+            now = head
+            while now is not None:
+                tmp = now.next
+                now.next = Node(now.val)
+                now.next.next = tmp
+                now = now.next.next
+            return head
+        def connect_node_random(head):
+            now = head
+            while now is not None:
+                n = now.next
+                if now.random == None:
+                    n.random = None
+                else:
+                    n.random = now.random.next
+                now = n.next
+            return head
+        def reconnect_node(head):
+            h = head.next
+            n = h
+            while n.next is not None:
+                n.next = n.next.next
+                n = n.next
+            return h
+        head = clone_node(head)
+        head = connect_node_random(head)
+        head = reconnect_node(head)
+        return head
+
+
